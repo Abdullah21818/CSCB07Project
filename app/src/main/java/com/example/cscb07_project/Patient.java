ba8fc7d46082dc5444dfe132d11fc5bd946a7339
FabricatedReality <57849707+FabricatedReality@users.noreply.github.com>
2021-07-29 16:24:18 -0400

    Added Date class

--- app/src/main/java/com/example/cscb07_project/Patient.java
+++ app/src/main/java/com/example/cscb07_project/Patient.java
@@ -5,17 +5,12 @@ package com.example.cscb07_project;
 import com.example.cscb07_project.data.model.User;
 
 public class Patient extends User {
-    private int month;
-    private int day;
-    private int year;
+    private Date birthday;
 
 
     public Patient(String userId, String name, int month, int day, int year){
         super(userId, name);
-        this.month=month;
-        this.day=day;
-        this.year = year;
-
+        birthday = new Date(month, day, year);
     }
 
 
