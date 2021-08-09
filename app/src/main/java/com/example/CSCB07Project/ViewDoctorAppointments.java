a2dfbc0b82650e71a7a8f512760111aba15c6fee
Abdullah Hasan <abdullahasan2181@gmail.com>
2021-08-09 16:13:41 -0400

    old appointments gone and visited updates

--- app/src/main/java/com/example/CSCB07Project/ViewDoctorAppointments.java
+++ app/src/main/java/com/example/CSCB07Project/ViewDoctorAppointments.java
@@ -10,9 +10,15 @@ import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.time.Year;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 
 public class ViewDoctorAppointments extends AppCompatActivity {
+    private String docUsername;
+    private String patUsername;
+    //private String patUsername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,8 @@ public class ViewDoctorAppointments extends AppCompatActivity {
 
         Intent intent = getIntent();
         String userId = intent.getStringExtra("userId");
+        docUsername = intent.getStringExtra("userId");
+        deleteAppointment();
 
         FirebaseAPI.getDoctor(userId, new Callback<HashMap<String, Object>>() {
             @Override
@@ -30,8 +38,8 @@ public class ViewDoctorAppointments extends AppCompatActivity {
 
                 LinearLayout layout = findViewById(R.id.appointLayout);
                 LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                                                    LinearLayout.LayoutParams.WRAP_CONTENT,
-                                                    LinearLayout.LayoutParams.WRAP_CONTENT);
+                        LinearLayout.LayoutParams.WRAP_CONTENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT);
                 int i = 1;
 
                 for (Appointment a : doctor.getUpcomingAppointments()) {
@@ -61,5 +69,62 @@ public class ViewDoctorAppointments extends AppCompatActivity {
 
     public void backToDashboard(View view) {
         this.finish();
+    }
+
+    public void deleteAppointment() {
+        FirebaseAPI.getDoctor(docUsername, new Callback<HashMap<String, Object>>() {
+            @Override
+            public void onCallback(HashMap<String, Object> data) {
+                Doctor doctor = new Doctor(data);
+
+                //get current month, day, and year
+                double curMonth = (double) (Calendar.getInstance().get(Calendar.MONTH) + 1.0) / 12.0;
+                double curDay = (double) (Calendar.getInstance().get(Calendar.DAY_OF_YEAR)) / 365.0;
+                double curHour = (double) (Calendar.getInstance().get(Calendar.HOUR_OF_DAY)) / 8760.0;
+                double curMin = (double) (Calendar.getInstance().get(Calendar.MINUTE)) / 525600.0;
+
+                double curDate = (double) Calendar.getInstance().get(Calendar.YEAR) + curMonth + curDay + curHour + curMin;
+
+
+                for (Appointment a : doctor.getUpcomingAppointments()) {
+                    double endYear = a.getEnd().getYear();
+                    double endMonth = (double) (a.getEnd().getMonth()) / 12.0;
+                    double endDay = (double) (a.getEnd().getDay()) / 365.0;
+                    double endHour = (double) (a.getEnd().getHour()) / 8760.0;
+                    double endMin = (double) (a.getEnd().getMinute()) / 525600.0;
+                    double endDate = endYear + endMonth + endDay + endHour + endMin;
+                    if (curDate >= endDate) {
+                        patUsername = a.getPatient();
+                        doctor.addVisited(patUsername);
+
+                        FirebaseAPI.getPatient(patUsername, new Callback<HashMap<String, Object>>() {
+                            @Override
+                            public void onCallback(HashMap<String, Object> data1) {
+                                Patient patient = new Patient(data1);
+                                patient.addVisited(docUsername);
+                                patient.removeAppointment(a);
+                                finish();
+                            }
+                        });
+                        doctor.removeAppointment(a);
+                    }
+                }
+
+            }
+//
+//                Appointment a = new Appointment(docUsername, patUsername, new Date(month, day, year
+//                        , SelectedHour,0), new Date(month, day, year, SelectedHour + 1,0));
+//                //Log.i("doctorInfo",doctor.getUpcomingAppointments().toString());
+//                //Log.i("Appointment", a.getDoctor() + " " + a.getPatient() + " " + a.getStart().toString() + " " + a.getEnd().toString());
+//                doctor.addAppointment(a);
+//                FirebaseAPI.getPatient(patUsername, new Callback<HashMap<String, Object>>() {
+//                    @Override
+//                    public void onCallback(HashMap<String, Object> data) {
+//                        Patient patient = new Patient(data);
+//                        patient.addAppointment(a);
+//                        finish();
+//                    }
+//                });
+        });
     }
 }
