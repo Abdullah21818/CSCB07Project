8cddffa8895a679c0b2a35ffe4bd0ced2758346a
Ricky <rickycheuk59@gmail.com>
2021-08-09 19:55:09 -0400

    Update ViewDoctorAppointments.java

--- app/src/main/java/com/example/CSCB07Project/ViewDoctorAppointments.java
+++ app/src/main/java/com/example/CSCB07Project/ViewDoctorAppointments.java
@@ -14,10 +14,14 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.Calendar;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ViewDoctorAppointments extends AppCompatActivity {
+    private String docUsername;
+    private String patUsername;
+    //private String patUsername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +30,8 @@ public class ViewDoctorAppointments extends AppCompatActivity {
 
         Intent intent = getIntent();
         String userId = intent.getStringExtra("userId");
+        docUsername = intent.getStringExtra("userId");
+        deleteAppointment();
 
         FirebaseAPI.getDoctor(userId, new Callback<HashMap<String, Object>>() {
             @Override
@@ -60,5 +66,62 @@ public class ViewDoctorAppointments extends AppCompatActivity {
 
     public void backToDashboard(View view) {
         this.finish();
+    }
+
+    public void deleteAppointment() {
+        FirebaseAPI.getDoctor(docUsername, new Callback<HashMap<String, Object>>() {
+            @Override
+            public void onCallback(HashMap<String, Object> data) {
+                Doctor doctor = new Doctor(data);
+
+                //get current month, day, and year
+                double curMonth = (double) (Calendar.getInstance().get(Calendar.MONTH) + 1.0) / 12.0;
+                double curDay = (double) (Calendar.getInstance().get(Calendar.DAY_OF_YEAR)) / 365.0;
+                double curHour = (double) (Calendar.getInstance().get(Calendar.HOUR_OF_DAY)) / 8760.0;
+                double curMin = (double) (Calendar.getInstance().get(Calendar.MINUTE)) / 525600.0;
+
+                double curDate = (double) Calendar.getInstance().get(Calendar.YEAR) + curMonth + curDay + curHour + curMin;
+
+
+                for (Appointment a : doctor.getUpcomingAppointments()) {
+                    double endYear = a.getEnd().getYear();
+                    double endMonth = (double) (a.getEnd().getMonth()) / 12.0;
+                    double endDay = (double) (a.getEnd().getDay()) / 365.0;
+                    double endHour = (double) (a.getEnd().getHour()) / 8760.0;
+                    double endMin = (double) (a.getEnd().getMinute()) / 525600.0;
+                    double endDate = endYear + endMonth + endDay + endHour + endMin;
+                    if (curDate >= endDate) {
+                        patUsername = a.getPatient();
+                        doctor.addVisited(patUsername);
+
+                        FirebaseAPI.getPatient(patUsername, new Callback<HashMap<String, Object>>() {
+                            @Override
+                            public void onCallback(HashMap<String, Object> data1) {
+                                Patient patient = new Patient(data1);
+                                patient.addVisited(docUsername);
+                                patient.removeAppointment(a);
+                                finish();
+                            }
+                        });
+                        doctor.removeAppointment(a);
+                    }
+                }
+
+            }
+//
+//                Appointment a = new Appointment(docUsername, patUsername, new Date(month, day, year
+//                        , SelectedHour,0), new Date(month, day, year, SelectedHour + 1,0));
+//                //Log.i("doctorInfo",doctor.getUpcomingAppointments().toString());
+//                //Log.i("Appointment", a.getDoctor() + " " + a.getPatient() + " " + a.getStart().toString() + " " + a.getEnd().toString());
+//                doctor.addAppointment(a);
+//                FirebaseAPI.getPatient(patUsername, new Callback<HashMap<String, Object>>() {
+//                    @Override
+//                    public void onCallback(HashMap<String, Object> data) {
+//                        Patient patient = new Patient(data);
+//                        patient.addAppointment(a);
+//                        finish();
+//                    }
+//                });
+        });
     }
 }
