5b6b69cb32559ff7bf521c944645e6aadc209351
Ricky <rickycheuk59@gmail.com>
2021-08-09 19:50:18 -0400

    Update ViewDoctorAppointments.java

--- app/src/main/java/com/example/CSCB07Project/ViewDoctorAppointments.java
+++ app/src/main/java/com/example/CSCB07Project/ViewDoctorAppointments.java
@@ -14,7 +14,6 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import java.util.Calendar;
 import java.util.ArrayList;
 import java.util.HashMap;
 
